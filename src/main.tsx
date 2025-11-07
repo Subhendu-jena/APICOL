63fcc1ca062bfb28a4dc0923f32ddecc6dc1fd83
“subhendu <subhendujena38@gmail.com>
2025-11-07 16:37:57 +0530

    added hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import ErrorBoundary from "./components/errorHandling/ErrorBoundry";
 import { Provider } from "react-redux";
 import { store } from "./store/store/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ErrorBoundary>
         <Provider store={store}>
           <App />
         </Provider>
       </ErrorBoundary>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
